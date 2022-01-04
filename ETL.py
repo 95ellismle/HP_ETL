30e58072e317a07a506a8c84228cec2b6be6ced6
Matt Ellis <matt.ellis@fairxchange.co.uk>
2022-01-04 08:17:57 +0000

    yaml to json

--- ETL.py
+++ ETL.py
@@ -3,8 +3,10 @@ import pyarrow.parquet as pq
 
 from collections import Counter
 from pathlib import Path
+from hp_config import path as config_path
 
 import logging
+import json
 import subprocess
 import sys
 
@@ -82,6 +84,22 @@ class ETL:
         LOG.info('Creating postcode data structure')
         self.postcode_data = self._postcode_reshape()
 
+        # Create mappings for later
+        LOG.info('Mapping streets to a postcode')
+        mapping = self._place_to_postcode('street')
+        LOG.info("Updating and writing map")
+        self._update_json(mapping, config_path / 'maps/street_to_pc.json')
+
+        LOG.info('Mapping cities to a postcode')
+        mapping = self._place_to_postcode('city')
+        LOG.info("Updating and writing map")
+        self._update_json(mapping, config_path / 'maps/city_to_pc.json')
+
+        LOG.info('Mapping counties to a postcode')
+        mapping = self._place_to_postcode('county')
+        LOG.info("Updating and writing map")
+        self._update_json(mapping, config_path / 'maps/county_to_pc.json')
+
         # Load
         LOG.info('Writing files')
         self._load(year)
@@ -98,16 +116,55 @@ class ETL:
         # Save the postcode data
         LOG.debug("Writing postcode files")
         for i in self.postcode_data:
-            fn = self._postcode_dir / f'{i}.parq'
+            # Create file structure
+            dir_name = self._postcode_dir / i[0]
+            fn = dir_name / f'{i[1]}.parq'
+            if not dir_name.is_dir():
+                dir_name.mkdir()
+
             df = self.postcode_data[i]
+
+            # Concat files if they already exist
             if fn.is_file():
                 df = pd.concat([pd.read_parquet(fn), df])
                 df = df.sort_values('date_transfer')
+
             df.to_parquet(fn)
 
         # Save standard data files
         LOG.debug("Writing data files")
         self.data.to_parquet(self._parq_fn)
+
+    def _update_json(self, data, json_filepath):
+        """Will update a json file with the contents of a dictionary
+
+        The dictionary should have the following structure:
+            {key1: [val1, val2, ..], key2: [...], ...}
+
+        If json exists will update, if it doesn't will create.
+
+        Args:
+            data: a dict with some data to update the json with
+            json_filepath: the filepath to the json file to write
+        """
+        # Read current json file
+        curr_data = {}
+        if json_filepath.is_file():
+            with open(json_filepath, 'r') as f:
+                curr_data = json.load(f)
+                if curr_data is None:
+                    curr_data = {}
+
+        # Update with newer data
+        for i in data:
+            if i in curr_data:
+                curr_data[i] = list(set(curr_data[i] + data[i]))
+            else:
+                curr_data[i] = data[i]
+
+        # Save the data
+        with open(json_filepath, 'w') as f:
+            json.dump(curr_data, f)
 
     def _postcode_reshape(self):
         """Create individual dfs for each unique beginning pair of letters."""
@@ -130,6 +187,31 @@ class ETL:
                 new_df = new_df[~mask]
 
         return d
+
+    def _place_to_postcode(self, col):
+        """Will create a dictionary mapping every unique occurance of a column to the
+           first 2 letters of a postcode.
+
+        This will then be appended to a yaml file in the config directory
+        """
+        df = self.data[self.data['postcode'] != 'nan']
+        df = df[['postcode', col]]
+        df['postcode'] = df['postcode'].str.slice(0, 2)
+
+        df = df.sort_values('postcode')
+        counts = Counter(df['postcode'])
+
+        c = 0
+        mapping = {}
+        for pc in sorted(counts):
+            new_df = df.iloc[c:c+counts[pc]]
+
+            for val in new_df[col].unique():
+                mapping.setdefault(val, []).append(pc)
+
+            c += counts[pc]
+
+        return mapping
 
     def _extract(self, year):
         """Will read the data file from wherever it can be found.
@@ -209,7 +291,7 @@ class ETL:
         return df
 
 
-for year in range(1995, 2022):
+for year in range(1995, 1996):
     LOG.info(f'Carrying out year: {year}')
     e = ETL(year)
 
